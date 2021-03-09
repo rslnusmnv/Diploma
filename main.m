d0433ce032f1fcdd6f3285186510c711a155a699
rslnusmnv <ruslan90470@gmail.com>
2021-03-10 00:52:16 +0500

    Добавил запись фреймов и аудиосемплов, но звука в конечном видео нет

--- main.m
+++ main.m
@@ -5,10 +5,10 @@ videoWR = vision.VideoFileWriter('Filename', 'new.avi', 'AudioInputPort', true);
 % videoPlr = vision.VideoPlayer; % создание плеера
 % videoFR.info() % Если раскомментить можно узнать инфу о видео
 %% READING FRAMES AND AUDIOSAMPLES (HARD VERSION)
-frameCounter = 0;
+frameCounter = 1;
 while ~isDone(videoFR)
     %frame = step(videoFR); %Чтение кадра без аудиосэмпла    
-    [frame, sample] = videoFR(); %Чтение кадра с аудиосэмплом  
+    [frame{frameCounter}, sample] = videoFR(); %Чтение кадра с аудиосэмплом  
     lengthAudiosample = length(sample);
 %     %% EXTRACT AUDIO (HARD VERSION)Раскомментируй 32 и закомментируй 31
 %     z = 1;
@@ -19,23 +19,21 @@ while ~isDone(videoFR)
 %     end
 %     %%
     %step(videoPlr, frame); %Воспроизведение кадров
-    videoWR(frame, sample);
     frameCounter = frameCounter + 1;
 end
 % figure; plot(audioHV); title('Аудио собранное по кадрам');
 % figure; plot(audioSV); title('Аудио из audioread');
-release(videoWR);
 %% READING AUDIO (SIMPLE VERSION)
 [audioSV, Fs] = audioread ( 'video.avi', 'double' );
 audioSV = audioSV(1:length(audioSV)); %Это чтобы оставить только одну дорожку аудио
-audioSV(length(audioSV) + 1:frameCounter * lengthAudiosample) = 0; %Это дополнение до целого числа кадров
+audioSV = audioSV(1:739200); %Это дополнение до целого числа кадров
 %% SPEECH COMPRESSION (PREPROCESSING)
 audio = audioSV;
 % audio = audioHV;
 n = 5;
 n1 = 3;
 L = length(audio);
-N = lengthAudiosample;
+N = 6*lengthAudiosample;
 P = L / N;
 for i=1:P
     audiosample{i} = audio(1+(N*(i-1)):N*i);
@@ -103,11 +101,24 @@ for i=1:P
     newAudiosample{xh(i)} = idctScrambleAudioSample{i};
 end
 %UNION CEIL
-newAudio = cell2mat(newAudiosample); 
+newAudio = cell2mat(newAudiosample);
 %DRAWING GRAPHICS
 figure; subplot (2,1,1); plot(audio); title('Исходное аудио');
 subplot (2,1,2); plot(newAudio); title('Аудио с встроенным ЦВЗ');
+audiowrite('newAudio.wav',newAudio, Fs);
 save('NewAudioMat.mat','newAudio');
+%% WRITING FRAMES AND AUDIOSAMPLES
+for m = 1:L/lengthAudiosample
+    newSample{m} = newAudio(1+(lengthAudiosample*(m-1)):lengthAudiosample*m);
+end
+frameCounter = 1;
+while frameCounter ~= L/lengthAudiosample
+    videoWR(frame{frameCounter}, newSample{frameCounter});
+    frameCounter = frameCounter + 1;
+end
+% figure; plot(audioHV); title('Аудио собранное по кадрам');
+% figure; plot(audioSV); title('Аудио из audioread');
+release(videoWR);
 clear audioSV Fs;
 
 
